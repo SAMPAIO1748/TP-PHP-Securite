a8a99f6911e3243ad4e51531779e4c5477f90141
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-12-15 16:45:55 +0100

    comment

--- show_post.php
+++ show_post.php
@@ -16,22 +16,24 @@
     $id = $_GET['id'];
 
     $bdd = new PDO('mysql:host=localhost;dbname=securite', 'root', 'root');
-    $sql = "SELECT * FROM post WHERE post.id = :id";
+    $sql = "SELECT * FROM post 
+    LEFT JOIN comment ON comment.id_post = post.id 
+    LEFT JOIN user ON comment.id_user = user.id  WHERE post.id = :id";
     $requete = $bdd->prepare($sql);
     //Avec bindValue on attribue une valeur à :id et on sécurise cette valeur en définissant des paramètres
     $requete->bindValue(':id', $id, PDO::PARAM_INT);
     $requete->execute();
-    $resultat = $requete->fetch();
+    $resultat = $requete->fetchAll();
     // On utilise fetch car on obtient un seul résultat.
 
-    //var_dump($resultat);
+    //var_dump($resultat[0]);
 
 
     ?>
 
-    <h1>Article n°<?php echo $resultat['id'] ?></h1>
+    <h1>Article n°<?php echo $resultat[0]['id'] ?></h1>
 
-    <img src="<?php echo "img/" . $resultat['img'] ?>" alt="<?php echo $resultat['title']  ?>">
+    <img src="<?php echo "img/" . $resultat[0]['img'] ?>" alt="<?php echo $resultat[0]['title']  ?>">
 
     <table>
         <thead>
@@ -45,14 +47,29 @@
             <?php
 
             // conversion date fomrat français (jour/mois/Année)
-            $date = date('d/m/Y', strtotime($resultat['date']));
+            $date = date('d/m/Y', strtotime($resultat[0]['date']));
 
-            echo "<tr><td>" . $resultat['title'] . "</td><td>" .
-                $resultat['content'] . "</td><td>" . $date . "</td></tr>"
+            echo "<tr><td>" . $resultat[0]['title'] . "</td><td>" .
+                $resultat[0]['content'] . "</td><td>" . $date . "</td></tr>"
 
             ?>
         </tbody>
     </table>
+
+    <hr>
+    <h2>Les commentaires</h2>
+
+    <?php
+
+    if (isset($resultat[0]['contenue'])) {
+        foreach ($resultat as $comment)
+            echo '<p>' . $comment['contenue'] . "</p><h3>Par " . $comment['name'] . " " .
+                $comment['firstname'] . "</h2><hr>";
+    }
+
+    ?>
+
+
 
 </body>
 
